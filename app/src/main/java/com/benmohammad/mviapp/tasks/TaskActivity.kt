b16fdda66e28c57156f9a8cc7ab1b4afdd8631a6
Ben Mohammad <benmohammad593@gmail.com>
2020-07-28 09:41:27 +0100

    Initial commit

---
+++ app/src/main/java/com/benmohammad/mviapp/tasks/TaskActivity.kt
@@ -0,0 +1,12 @@
+package com.benmohammad.mviapp.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.benmohammad.mviapp.R
+
+class TaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_act)
+    }
+}
